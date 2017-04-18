3d9090fd214491d7a7f5f5911a7eb86240793d74
Rita <ritamukimba@gmail.com>
2017-04-18 14:41:51 +0300

    FizzBuzz Lab

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,43 +1,42 @@
 import unittest
-from prime import generate_prime
 
-class PrimeNumberTestCases(unittest.TestCase):
+class TestPrimeNumberGenerator(unittest.TestCase):
 
-    def test_for_correct_answer_including_number(self):
-        result = generate_prime(11)
-        self.assertEqual(result, [1,2,3,5,7,11] , msg='Wrong answers')
+    def test_if_int(self):
+        self.assertIsNot(generate_prime_numbers("digits"),"range is not integer")
 
-    def test_for_another_right_answer_without_number(self):
-        result = generate_prime(20)
-        self.assertEqual(result, [1,2,3,5,7,11,13,17,19] , msg='Wrong answers')
+    def test_if_positive(self):
+        self.assertIsNot(generate_prime_numbers(-1),"range is a negative number")
+
+    def test_prime_numbers(self):
+        self.assertEqual(generate_prime_numbers(10),[2,3,5,7])
 
     def test_only_list_returned(self):
-        result = generate_prime(9)
+        result = generate_prime_numbers(9)
         self.assertEqual(type(result), list , msg='Invalid output')
+    
 
-    def test_for_not_allowing_non_integer(self):
-        with self.assertRaises(ValueError) as context:
-            generate_prime('string')
-            self.assertEqual(
-                "The provided input is not an integer.",
-                context.exception.message, "Invalid input not allowed"
-            )
 
-    def test_to_check_that_all_items_in_list_are_integers(self):
-        result = generate_prime(20)
-        self.assertTrue(all(isinstance(n, int) for n in result), msg="Some of the items in the list are not integers")
+def generate_prime_numbers(n):
+    prime_numbers = []
+    test_numbers = [2,3]
+    if isinstance(n, int):
+        if n > 0:
+            for num in range(2, n):
+                if num in test_numbers:
+                    prime_numbers.append(num)
+                else:
+                    is_prime = True
+                    for test_number in test_numbers:
+                        if num % test_number == 0:
+                            is_prime = False
+                    if is_prime == True:
+                        prime_numbers.append(num)
+            return prime_numbers
+        else:
+            return "range is a negative number"
+    else:
+        return "range is not integer"
 
-    def test_for_negative_values(self):
-        result = generate_prime(-3)
-        self.assertEqual(result, [], msg='It should return an empty list for negative times.')
-
-    def test_for_when_no_variable_is_passed(self):
-        with self.assertRaises(ValueError) as context:
-            generate_prime()
-            self.assertEqual(
-                "The provided input is not an integer.",
-                context.exception.message, "Invalid input not allowed"
-            )
-
-if __name__ == '__main__' : unittest.main()
-class PrimeNumberTestCases(unittest.TestCase):
+if __name__ == '__main__':
+    unittest.main()
--- test.py
+++ test.py
@@ -1,9 +1,36 @@
 import unittest
-from math import sqrt
-from app.generate_prime_numbers import *
- 
-class TestGeneratePrimeNumbers(unittest.TestCase):
-   
-    def setUp(self):
-        self.arg_limit = 32
-        self.sample_prime = [2,3,5,7,<span style="font-size:10.5pt;fo</blockquote></div>
+
+class TestPrimeNumberGenerator(unittest.TestCase):
+
+    def test_int(self):
+        self.assertIsNot(generate_prime_numbers("stuff"),"range is not integer")
+
+    def test_not_negative(self):
+        self.assertIsNot(generate_prime_numbers(-1),"range is a negative number")
+
+    def test_prime_numbers(self):
+        self.assertEqual(generate_prime_numbers(10),[2,3,5,7])
+
+
+def generate_prime_numbers(n):
+    prime_numbers = []
+    test_numbers = [2,3]
+    if isinstance(n, int):
+        if n > 0:
+            for num in range(2, n):
+                if num in test_numbers:
+                    prime_numbers.append(num)
+                else:
+                    is_prime = True
+                    for test_number in test_numbers:
+                        if num % test_number == 0:
+                            is_prime = False
+                    if is_prime == True:
+                        prime_numbers.append(num)
+            return prime_numbers
+        else:
+            return "range is a negative number"
+    else:
+        return "range is not integer"
+
+if __name__ == '__ma
