f32f4fc671c970b6e4bb8e4b6e8375475cde90b3
Rita <ritamukimba@gmail.com>
2017-04-18 13:00:38 +0300

    imported function for prime number

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,21 +1,43 @@
-def primes_numbers(x): 
-	if x==2: return [2]
-	elif x<2: return []
-	s=range(3,x+1,2)
-	mroot = x ** 0.5
-	half=(x+1)/2-1
-	i=0
-	m=3
-	while m <= mroot:
-		if s[i]:
-			j=(m*m-3)/2
-			s[j]=0
-			while j<half:
-				s[j]=0
-				j+=m
-		i=i+1
-		m=2*i+3
-	return [2]+[x for x in s if x]
+import unittest
+from prime import generate_prime
 
-print primes_numbers(12)
-#print primes(3000)
+class PrimeNumberTestCases(unittest.TestCase):
+
+    def test_for_correct_answer_including_number(self):
+        result = generate_prime(11)
+        self.assertEqual(result, [1,2,3,5,7,11] , msg='Wrong answers')
+
+    def test_for_another_right_answer_without_number(self):
+        result = generate_prime(20)
+        self.assertEqual(result, [1,2,3,5,7,11,13,17,19] , msg='Wrong answers')
+
+    def test_only_list_returned(self):
+        result = generate_prime(9)
+        self.assertEqual(type(result), list , msg='Invalid output')
+
+    def test_for_not_allowing_non_integer(self):
+        with self.assertRaises(ValueError) as context:
+            generate_prime('string')
+            self.assertEqual(
+                "The provided input is not an integer.",
+                context.exception.message, "Invalid input not allowed"
+            )
+
+    def test_to_check_that_all_items_in_list_are_integers(self):
+        result = generate_prime(20)
+        self.assertTrue(all(isinstance(n, int) for n in result), msg="Some of the items in the list are not integers")
+
+    def test_for_negative_values(self):
+        result = generate_prime(-3)
+        self.assertEqual(result, [], msg='It should return an empty list for negative times.')
+
+    def test_for_when_no_variable_is_passed(self):
+        with self.assertRaises(ValueError) as context:
+            generate_prime()
+            self.assertEqual(
+                "The provided input is not an integer.",
+                context.exception.message, "Invalid input not allowed"
+            )
+
+if __name__ == '__main__' : unittest.main()
+class PrimeNumberTestCases(unittest.TestCase):
---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from math import sqrt
+from app.generate_prime_numbers import *
+ 
+class TestGeneratePrimeNumbers(unittest.TestCase):
+   
+    def setUp(self):
+        self.arg_limit = 32
+        self.sample_prime = [2,3,5,7,<span style="font-size:10.5pt;fo</blockquote></div>
